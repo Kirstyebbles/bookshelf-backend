4ba4cea2ce82d9c116fef40e8c892bd7e8b48e92
Kirstyebbles <kirsty.ebbles@yorksj.ac.uk>
2026-05-18 16:07:49 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,24 +1,27 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.log("DB CONNECTION ERROR CODE:", err.code);
     console.log("DB CONNECTION ERROR MESSAGE:", err.message);
-    console.log("DB CONNECTION ERROR SQL:", err.sqlMessage);
   } else {
-    console.log("MySQL Connected");
+    console.log("MySQL pool connected");
+    connection.release();
   }
 });
 
